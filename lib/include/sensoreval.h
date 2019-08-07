87a37d18a6f132f43c3b8014b6f931f8ab6471a2
M1cha <sigmaepsilon92@gmail.com>
2019-08-07 10:29:31 +0200

    lib: add sensoreval_assert_se

--- lib/include/sensoreval.h
+++ lib/include/sensoreval.h
@@ -4,6 +4,7 @@
 #include <cairo/cairo.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 #include <sensoreval/data.h>
 #include <sensoreval/config.h>
@@ -11,5 +12,13 @@
 #include <sensoreval/render.h>
 #include <sensoreval/math.h>
 #include <sensoreval/plot.h>
+
+#define sensoreval_unlikely(x) (__builtin_expect(!!(x), 0))
+#define sensoreval_assert_se(expr) do { \
+    if (sensoreval_unlikely(!(expr))) { \
+        fprintf(stderr, "Assertion '%s' failed at %s:%u. Aborting.\n", #expr , __FILE__, __LINE__); \
+        abort(); \
+    } \
+} while(0)
 
 #endif /* SENSOREVAL_H */
