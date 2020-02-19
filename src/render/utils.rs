e197b6a5991dd4cb4426c157f2059f873b6d6b22
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-02-19 07:55:51 +0100

    render/utils: turn render_graph into a struct

--- src/render/utils.rs
+++ src/render/utils.rs
@@ -73,76 +73,107 @@ pub fn surface_sz_user(cr: &cairo::Context) -> (f64, f64) {
     cr.device_to_user_distance(sw, sh)
 }
 
-#[allow(clippy::too_many_arguments)]
-pub fn draw_graph<T: Iterator<Item = u64>, D: Iterator<Item = f64>>(
-    cr: &cairo::Context,
-    iter_time: &mut T,
-    iter_data: &mut D,
-    graph_width: f64,
-    graph_height: f64,
-    dt: u64,
-    maxval: f64,
-    redval: f64,
-) {
-    cr.save();
-    cr.rectangle(0.0, 0.0, graph_width, graph_height);
-    cr.clip();
+pub struct Graph {
+    pub width: f64,
+    pub height: f64,
+    pub dt: u64,
+    pub maxval: f64,
+    pub redval: f64,
+    pub line_width: f64,
+    pub border_color: u32,
+    pub border_width: f64,
+}
 
-    // graph-line style
-    cr.set_line_width(6.0);
-    let pat = cairo::LinearGradient::new(
-        0.0,
-        graph_height - (graph_height / maxval * redval),
-        0.0,
-        graph_height,
-    );
-    pat.add_color_stop_rgb(0.0, 1.0, 0.0, 0.0);
-    pat.add_color_stop_rgb(0.5, 1.0, 1.0, 0.0);
-    pat.add_color_stop_rgb(1.0, 0.0, 1.0, 0.0);
-    cr.set_source(&pat);
-
-    let mut tnow: u64 = 0;
-    let mut tstart: u64 = 0;
-    let mut first: bool = true;
-
-    // clippy is wrong, because we're iterating of two iterators at the same time
-    // alos, I'm not comfortable using zip, yet due to unclear performance impacts
-    #[allow(clippy::while_let_loop)]
-    loop {
-        let time = unwrap_opt_or!(iter_time.next(), break);
-        let data = unwrap_opt_or!(iter_data.next(), break);
-        let was_first = first;
-        if first {
-            tnow = time;
-            tstart = match time.checked_sub(dt) {
-                None => 0,
-                Some(v) => v,
-            };
-            first = false;
-        }
-
-        if time < tstart {
-            break;
-        }
-
-        let x = graph_width - (graph_width / (dt as f64) * ((tnow - time) as f64));
-        let y = graph_height - (graph_height / maxval * data);
-
-        if was_first {
-            cr.move_to(x, y);
-        } else {
-            cr.line_to(x, y);
+impl Default for Graph {
+    fn default() -> Self {
+        Self {
+            width: 2.0,
+            height: 1.0,
+            dt: 10_000_000,
+            maxval: 1.0,
+            redval: 2.0,
+            line_width: 1.0,
+            border_color: 0x0000_00ff,
+            border_width: 1.0,
         }
     }
+}
 
-    cr.stroke();
-    cr.restore();
+impl Graph {
+    pub fn draw<T: Iterator<Item = u64>, D: Iterator<Item = f64>>(
+        &self,
+        cr: &cairo::Context,
+        iter_time: &mut T,
+        iter_data: &mut D,
+    ) -> f64 {
+        let (cx, cy) = cr.get_current_point();
 
-    // border
-    render::utils::set_source_rgba_u32(cr, 0x0000_00ff);
-    cr.set_line_width(3.0);
-    cr.move_to(0.0, 0.0);
-    cr.line_to(0.0, graph_height);
-    cr.line_to(graph_width, graph_height);
-    cr.stroke();
+        cr.save();
+        cr.rectangle(cx, cy, self.width, self.height);
+        cr.clip();
+
+        // graph-line style
+        cr.set_line_width(self.line_width);
+        let pat = cairo::LinearGradient::new(
+            0.0,
+            self.height - (self.height / self.maxval * self.redval),
+            0.0,
+            self.height,
+        );
+        pat.add_color_stop_rgb(0.0, 1.0, 0.0, 0.0);
+        pat.add_color_stop_rgb(0.5, 1.0, 1.0, 0.0);
+        pat.add_color_stop_rgb(1.0, 0.0, 1.0, 0.0);
+        cr.set_source(&pat);
+
+        let mut tnow: u64 = 0;
+        let mut tstart: u64 = 0;
+        let mut first: bool = true;
+        let mut data_now: f64 = 0.0;
+
+        // clippy is wrong, because we're iterating of two iterators at the same time
+        // alos, I'm not comfortable using zip, yet due to unclear performance impacts
+        #[allow(clippy::while_let_loop)]
+        loop {
+            let time = unwrap_opt_or!(iter_time.next(), break);
+            let data = unwrap_opt_or!(iter_data.next(), break);
+            let was_first = first;
+            if first {
+                tnow = time;
+                tstart = match time.checked_sub(self.dt) {
+                    None => 0,
+                    Some(v) => v,
+                };
+                first = false;
+                data_now = data;
+            }
+
+            if time < tstart {
+                break;
+            }
+
+            let x = cx + self.width - (self.width / (self.dt as f64) * ((tnow - time) as f64));
+            let y = cy + self.height - (self.height / self.maxval * data);
+
+            if was_first {
+                cr.move_to(x, y);
+            } else {
+                cr.line_to(x, y);
+            }
+        }
+
+        cr.stroke();
+        cr.restore();
+
+        // border
+        cr.save();
+        render::utils::set_source_rgba_u32(cr, self.border_color);
+        cr.set_line_width(self.border_width);
+        cr.move_to(cx, cy);
+        cr.rel_line_to(0.0, self.height);
+        cr.rel_line_to(self.width, 0.0);
+        cr.stroke();
+        cr.restore();
+
+        data_now
+    }
 }
