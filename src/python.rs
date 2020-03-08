9054a08cdd487981105d20ca63fa4619617d34f1
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-03-08 08:40:07 +0100

    python: add new_args

--- src/python.rs
+++ src/python.rs
@@ -5,18 +5,15 @@ pub struct Python {
 }
 
 impl Python {
-    pub fn new<T: serde::ser::Serialize>(code: &T) -> Result<Self, Error> {
-        let mut child = std::process::Command::new("python")
-            .args(&[
-                "-c",
-                "\
-                 import sys\n\
-                 import pickle\n\
-                 def load_data():\n\
-                     \treturn pickle.load(sys.stdin.buffer)\n\
-                 exec(load_data())\n\
-                 ",
-            ])
+    pub fn new_args<I, Sp, Sa, T>(program: Sp, args: I, code: &T) -> Result<Self, Error>
+    where
+        I: IntoIterator<Item = Sa>,
+        Sa: AsRef<std::ffi::OsStr>,
+        Sp: AsRef<std::ffi::OsStr>,
+        T: serde::ser::Serialize,
+    {
+        let mut child = std::process::Command::new(program)
+            .args(args)
             .stdin(std::process::Stdio::piped())
             .stdout(std::process::Stdio::inherit())
             .spawn()?;
@@ -25,6 +22,23 @@ impl Python {
         serde_pickle::to_writer(stdin, &code, true)?;
 
         Ok(Self { child })
+    }
+
+    pub fn new<T: serde::ser::Serialize>(code: &T) -> Result<Self, Error> {
+        Self::new_args(
+            "python",
+            &[
+                "-c",
+                "\
+                 import sys\n\
+                 import pickle\n\
+                 def load_data():\n\
+                     \treturn pickle.load(sys.stdin.buffer)\n\
+                 exec(load_data())\n\
+                 ",
+            ],
+            code,
+        )
     }
 
     pub fn write<T: serde::ser::Serialize>(&mut self, value: &T) -> Result<(), Error> {
