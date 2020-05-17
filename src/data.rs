838206b82e0ba27121d85d7bc7a9e3483b401b02
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-05-17 16:00:07 +0200

    data: remove downscale

--- src/data.rs
+++ src/data.rs
@@ -1,5 +1,3 @@
-use crate::*;
-
 /// processed data sample
 #[derive(Debug)]
 pub struct Data {
@@ -74,76 +72,4 @@ pub fn id_for_time(dataset: &[Data], startid: usize, us: u64) -> Option<usize> {
     }
 
     None
-}
-
-pub fn downscale(lores: &mut Vec<Data>, dataset: &[Data], timeframe: u64) -> Result<(), Error> {
-    let data_last = unwrap_opt_or!(dataset.last(), return Err(Error::SampleNotFound));
-    let lores_len = data_last.time / timeframe;
-    let mut j = 0;
-
-    for i in 0..(lores_len as usize) {
-        let mut nsamples: usize = 0;
-        let mut data_lores = Data::default();
-
-        data_lores.time = (i as u64) * timeframe + timeframe / 2;
-
-        // sum up all data
-        while let Some(data) = dataset.get(j) {
-            if data.time >= (i as u64) * timeframe {
-                break;
-            }
-            j += 1;
-
-            for k in 0..3 {
-                data_lores.accel[k] += data.accel[k];
-                data_lores.gyro[k] += data.gyro[k];
-                data_lores.mag[k] += data.mag[k];
-            }
-
-            data_lores.temperature += data.temperature;
-            data_lores.pressure += data.pressure;
-
-            nsamples += 1;
-        }
-
-        if nsamples > 0 {
-            // calculate the mean values
-            for k in 0..3 {
-                data_lores.accel[k] /= nsamples as f64;
-                data_lores.gyro[k] /= nsamples as f64;
-                data_lores.mag[k] /= nsamples as f64;
-            }
-
-            data_lores.temperature /= nsamples as f64;
-            data_lores.pressure /= nsamples as f64;
-        } else if i > 0 {
-            // use the previous sample
-            let prev = dataset.get(i - 1).unwrap();
-
-            for k in 0..3 {
-                data_lores.accel[k] = prev.accel[k];
-                data_lores.gyro[k] = prev.gyro[k];
-                data_lores.mag[k] = prev.mag[k];
-            }
-
-            data_lores.temperature = prev.temperature;
-            data_lores.pressure = prev.pressure;
-        } else {
-            // use the first sample
-            let first = dataset.get(i).unwrap();
-
-            for k in 0..3 {
-                data_lores.accel[k] = first.accel[k];
-                data_lores.gyro[k] = first.gyro[k];
-                data_lores.mag[k] = first.mag[k];
-            }
-
-            data_lores.temperature = first.temperature;
-            data_lores.pressure = first.pressure;
-        }
-
-        lores.push(data_lores);
-    }
-
-    Ok(())
 }
