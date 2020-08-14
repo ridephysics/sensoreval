5b91fa11e034a6fabd6832cd04124e516596a244
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-08-14 18:35:02 +0200

    sensoreval_graphics: pendulum_2d: stop overriding color

--- sensoreval_graphics/src/pendulum_2d.rs
+++ sensoreval_graphics/src/pendulum_2d.rs
@@ -9,12 +9,7 @@ pub fn draw(cr: &cairo::Context, m1a: f64) {
     cr.translate(ssz.0 / 2.0, ssz.1 / 2.0);
     cr.rotate(m1a);
 
-    // clear
-    cr.set_source_rgba_u32(0x00000000);
-    cr.clear();
-
     // rod
-    cr.set_source_rgba_u32(0xffffffff);
     cr.set_line_width(10.0);
     cr.move_to(0.0, 0.0);
     cr.line_to(0.0, m1y);
@@ -23,7 +18,6 @@ pub fn draw(cr: &cairo::Context, m1a: f64) {
     // m1
     cr.save();
     cr.translate(0.0, m1y);
-    cr.set_source_rgba_u32(0xffffffff);
     cr.arc(0.0, 0.0, m1r, 0.0, 2.0 * std::f64::consts::PI);
     cr.fill();
     cr.restore();
