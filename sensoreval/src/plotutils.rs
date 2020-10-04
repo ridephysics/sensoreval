f9f9a99dce79f3e5ab20332ffcaa247a28441e88
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-10-04 08:45:20 +0200

    sensoreval: plotutils: add norm-accel plot

--- sensoreval/src/plotutils.rs
+++ sensoreval/src/plotutils.rs
@@ -1,5 +1,6 @@
 use crate::Data;
 use crate::Error;
+use ndarray_linalg::Norm;
 
 pub trait PlotUtils {
     fn add_measurements(&mut self, samples: &[Data], x: &[f64]) -> Result<(), Error>;
@@ -31,6 +32,17 @@ impl<'a> PlotUtils for sensoreval_utils::Plot<'a> {
         for i in 0..3 {
             let y: Vec<f64> = samples.iter().map(|s| s.mag[i]).collect();
             add_row("mag", i, &y)?;
+        }
+
+        {
+            let y: Vec<f64> = samples.iter().map(|s| s.accel.norm_l2()).collect();
+
+            let mut trace = Self::default_line();
+            trace.x(&x).y(&y).name("norm-a");
+            trace.line().color(sensoreval_utils::COLOR_M);
+
+            let rowid = self.ensure_row("norm-a")?;
+            self.add_trace_to_rowid(&mut trace, rowid)?;
         }
 
         Ok(())
