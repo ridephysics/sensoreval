59da674cfc79757dc167d83002310e6060ff8988
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-10-11 13:51:36 +0200

    sensoreval_psim: add impl_model_inner

--- sensoreval_psim/src/lib.rs
+++ sensoreval_psim/src/lib.rs
@@ -6,23 +6,29 @@ pub trait Model {
     fn dt(&self) -> f64;
 }
 
+macro_rules! impl_model_inner {
+    ($field:ident) => {
+        fn step<S>(&mut self, x: &mut ndarray::ArrayBase<S, ndarray::Ix1>)
+        where
+            S: ndarray::DataMut<Elem = f64>,
+        {
+            self.$field.iterate(x);
+        }
+
+        fn set_dt(&mut self, dt: f64) {
+            self.$field.set_dt(dt);
+        }
+
+        fn dt(&self) -> f64 {
+            self.$field.get_dt()
+        }
+    };
+}
+
 macro_rules! impl_model {
     ($type:ty, $field:ident) => {
         impl crate::Model for $type {
-            fn step<S>(&mut self, x: &mut ndarray::ArrayBase<S, ndarray::Ix1>)
-            where
-                S: ndarray::DataMut<Elem = f64>,
-            {
-                self.$field.iterate(x);
-            }
-
-            fn set_dt(&mut self, dt: f64) {
-                self.$field.set_dt(dt);
-            }
-
-            fn dt(&self) -> f64 {
-                self.$field.get_dt()
-            }
+            impl_model_inner!($field);
         }
     };
 }
