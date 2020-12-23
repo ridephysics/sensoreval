fa2274c8973a77c664666eefeab5a055f303fc0b
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-12-23 13:51:09 +0100

    sensoreval: config: introduce TimedArray

--- sensoreval/src/config.rs
+++ sensoreval/src/config.rs
@@ -269,6 +269,46 @@ pub struct SensorStdev {
     pub mag: SensorStdevXYZ,
 }
 
+struct TimedArray<'a> {
+    id: usize,
+    data: &'a [Vec<f64>],
+    next_data_time: Option<f64>,
+}
+
+impl<'a> TimedArray<'a> {
+    pub fn new(data: &'a [Vec<f64>]) -> Self {
+        let mut o = Self {
+            id: 0,
+            data,
+            next_data_time: None,
+        };
+        o.update_next();
+        o
+    }
+
+    pub fn next(&mut self, t: f64) -> Option<&[f64]> {
+        if let Some(next_data_time) = self.next_data_time {
+            if t >= next_data_time {
+                let ret = Some(&self.data[self.id][1..]);
+
+                self.id += 1;
+                self.update_next();
+                return ret;
+            }
+        }
+
+        None
+    }
+
+    fn update_next(&mut self) {
+        self.next_data_time = if self.id >= self.data.len() {
+            None
+        } else {
+            Some(self.data[self.id][0])
+        };
+    }
+}
+
 impl Config {
     fn add_noise<S, R>(arr: &mut ndarray::ArrayBase<S, ndarray::Ix1>, cfg: &NoiseXYZ, rng: &mut R)
     where
@@ -294,22 +334,13 @@ impl Config {
         sensoreval_psim::utils::rotate_imudata(rot, &mut sample.gyro);
     }
 
-    fn next_control_input_time(d: &SimulatorData, id: usize) -> Option<f64> {
-        if id >= d.control_input.len() {
-            None
-        } else {
-            Some(d.control_input[id][0])
-        }
-    }
-
     fn load_data_sim(d: &SimulatorData) -> Result<Vec<crate::Data>, Error> {
         let mut model = d.model.to_model_enum(d.dt);
         let nsamples = (d.duration / d.dt) as usize;
         let mut ret = Vec::new();
 
         let mut x = ndarray::Array::from(d.initial.clone());
-        let mut ciid = 0;
-        let mut next_input_time = Self::next_control_input_time(d, ciid);
+        let mut timed_array = TimedArray::new(&d.control_input);
 
         for id in 0..nsamples {
             let t = id as f64 * d.dt + d.start_off;
@@ -326,13 +357,8 @@ impl Config {
             model.to_gyro(&x, &mut sample.gyro);
             ret.push(sample);
 
-            if let Some(nit) = next_input_time {
-                if t + d.dt >= nit {
-                    model.set_control_input(Some(&d.control_input[ciid][1..]));
-
-                    ciid += 1;
-                    next_input_time = Self::next_control_input_time(d, ciid);
-                }
+            if let Some(control_input) = timed_array.next(t + d.dt) {
+                model.set_control_input(Some(control_input));
             }
 
             model.step(&mut x);
