d393ec67e5a714a6b88bb57e5c9eb6fa5f706629
Michael Zimmermann <sigmaepsilon92@gmail.com>
2020-12-29 05:36:00 +0100

    tools/booster_analysis: big improvements
    - we're preferring cpu time over memory efficiency, so consistently
      collect into Vecs. This also allows to make them non-mut.
    - use optional median filters for certain values
    - add simulator
    - add a few more plots

--- tools/booster_analysis/src/main.rs
+++ tools/booster_analysis/src/main.rs
@@ -2,6 +2,7 @@ use itertools::Itertools;
 use sensoreval::PlotUtils;
 use sensoreval_psim::models::booster::State;
 use sensoreval_psim::Model;
+use sensoreval_psim::ToImuSample;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -130,22 +131,39 @@ struct PlotOpt {
 }
 
 #[derive(serde::Deserialize)]
+#[serde(deny_unknown_fields)]
 struct PlotConfig {
+    // input data
+    mainconfig: String,
     #[serde(default)]
     startoff: u64,
     #[serde(default)]
     endoff: Option<u64>,
+    #[serde(default)]
+    accelx_winsz: usize,
+    #[serde(default)]
+    accely_winsz: usize,
+    #[serde(default)]
+    theta0d_winsz: usize,
+    #[serde(default)]
+    theta0dd_winsz: usize,
+    #[serde(default)]
+    diff_winsz: usize,
 
+    // basic physical parameters
     thetabd: f64,
     rb: f64,
-    friction: Option<f64>,
-
     thetas: f64,
     rs: f64,
 
+    // user-approximated physical parameters
     thetac: Option<f64>,
+    friction: Option<f64>,
+    rc: Option<f64>,
 
-    mainconfig: String,
+    // sim
+    #[serde(default)]
+    stateupdates: Vec<Vec<f64>>,
 }
 
 fn calc_theta0_one<F>(accel_needle: f64, calc_accel: F, other: Option<f64>) -> Option<f64>
@@ -181,7 +199,7 @@ where
     best.map(|best| best.0)
 }
 
-pub fn calc_theta0_all<F>(accel_needle: f64, calc_accel: F) -> Vec<f64>
+fn calc_theta0_all<F>(accel_needle: f64, calc_accel: F) -> Vec<f64>
 where
     F: Fn(f64) -> f64,
 {
@@ -196,6 +214,31 @@ where
     }
 
     results
+}
+
+trait VecInplace<T> {
+    fn resize_inplace(self, newlen: usize, value: T) -> Self;
+}
+
+impl<T: Clone> VecInplace<T> for Vec<T> {
+    fn resize_inplace(mut self, newlen: usize, value: T) -> Self {
+        self.resize(newlen, value);
+        self
+    }
+}
+
+trait MedianFilterEx<T> {
+    fn consume_ex(&mut self, value: T) -> T;
+}
+
+impl<T: Clone + PartialOrd> MedianFilterEx<T> for median::Filter<T> {
+    fn consume_ex(&mut self, value: T) -> T {
+        if self.len() > 0 {
+            self.consume(value)
+        } else {
+            value
+        }
+    }
 }
 
 fn plot(plotopt: &PlotOpt) {
@@ -217,44 +260,67 @@ fn plot(plotopt: &PlotOpt) {
     cfg.video.startoff = opt.startoff;
     cfg.video.endoff = opt.endoff;
     cfg.hud.renderer = sensoreval::config::HudRenderer::Generic;
+    let cfg = cfg;
     let samples = cfg.load_data().expect("can't read samples");
 
-    let mut iir_theta0dd = math::IIR::new(16.0);
-    let mut iir_theta0 = math::IIR::new(16.0);
-    let mut y_thetab = Vec::with_capacity(samples.len());
-    let mut y_accelx = Vec::with_capacity(samples.len());
-    let mut y_accely = Vec::with_capacity(samples.len());
-    let mut y_theta0l = Vec::with_capacity(samples.len());
-    let mut y_theta0h = Vec::with_capacity(samples.len());
-    let mut y_theta0d = Vec::with_capacity(samples.len());
-    let mut y_theta0dd = Vec::with_capacity(samples.len());
-    let mut y_theta0xl = Vec::with_capacity(samples.len());
-    let mut y_theta0xh = Vec::with_capacity(samples.len());
+    let y_thetab: Vec<_> = samples
+        .iter()
+        .map(|s| {
+            math::normalize_angle(
+                (s.time_seconds() - cfg.video.startoff as f64 / 1000.0) * opt.thetabd,
+            )
+        })
+        .collect();
+    let y_accelx = samples
+        .iter()
+        .scan(median::Filter::new(opt.accelx_winsz), |filter, s| {
+            Some(filter.consume_ex(s.accel[1]))
+        })
+        .skip(opt.accelx_winsz / 2)
+        .collect::<Vec<_>>()
+        .resize_inplace(samples.len(), 0.0);
+    let y_accely = samples
+        .iter()
+        .scan(median::Filter::new(opt.accely_winsz), |filter, s| {
+            Some(filter.consume_ex(s.accel[2]))
+        })
+        .skip(opt.accely_winsz / 2)
+        .collect::<Vec<_>>()
+        .resize_inplace(samples.len(), 0.0);
+    let y_theta0d = samples
+        .iter()
+        .scan(median::Filter::new(opt.theta0d_winsz), |filter, s| {
+            Some(filter.consume_ex(s.gyro[0]))
+        })
+        .skip(opt.theta0d_winsz / 2)
+        .collect::<Vec<_>>()
+        .resize_inplace(samples.len(), 0.0);
+    let y_theta0dd = samples
+        .iter()
+        .enumerate()
+        .scan(median::Filter::new(opt.theta0dd_winsz), |filter, (i, s)| {
+            Some(filter.consume_ex(if i == 0 {
+                0.0
+            } else {
+                (y_theta0d[i - 1] - y_theta0d[i])
+                    / (samples[i - 1].time_seconds() - s.time_seconds())
+            }))
+        })
+        .skip(opt.theta0dd_winsz / 2)
+        .collect::<Vec<_>>()
+        .resize_inplace(samples.len(), 0.0);
 
     // estimate x1/theta0 for all samples
-    for (i, s) in samples.iter().enumerate() {
-        let theta0d = s.gyro[0];
-        y_theta0d.push(theta0d);
-
-        let accelx = s.accel[1];
-        y_accelx.push(accelx);
-
-        let accely = s.accel[2];
-        y_accely.push(accely);
-
-        let thetab = math::normalize_angle(
-            (s.time_seconds() - cfg.video.startoff as f64 / 1000.0) * opt.thetabd,
-        );
-        y_thetab.push(thetab);
-
-        let theta0dd = if i == 0 {
-            0.0
-        } else {
-            (y_theta0d[i - 1] - theta0d)
-                / (samples[i - 1].time_seconds() - samples[i].time_seconds())
-        };
-        let theta0dd = iir_theta0dd.next(theta0dd);
-        y_theta0dd.push(theta0dd);
+    let mut y_theta0l = Vec::with_capacity(samples.len());
+    let mut y_theta0h = Vec::with_capacity(samples.len());
+    let mut y_theta0xl = Vec::with_capacity(samples.len());
+    let mut y_theta0xh = Vec::with_capacity(samples.len());
+    for (i, _) in samples.iter().enumerate() {
+        let thetab = y_thetab[i];
+        let theta0d = y_theta0d[i];
+        let accelx = y_accelx[i];
+        let accely = y_accely[i];
+        let theta0dd = y_theta0dd[i];
 
         let all_x = calc_theta0_all(accelx, |theta0| {
             -opt.rb * opt.thetabd.powi(2) * (thetab - theta0).sin()
@@ -285,7 +351,6 @@ fn plot(plotopt: &PlotOpt) {
                 .min_by(|x, y| x.0.partial_cmp(&y.0).unwrap())
                 .unwrap();
             let best = (all[best_a] + all[best_b]) / 2.0;
-            let best = iir_theta0.next(best);
             y_theta0l.push(best);
         } else {
             y_theta0xl.push(
@@ -315,6 +380,10 @@ fn plot(plotopt: &PlotOpt) {
             );
         }
     }
+    let y_theta0l = y_theta0l;
+    let y_theta0h = y_theta0h;
+    let y_theta0xl = y_theta0xl;
+    let y_theta0xh = y_theta0xh;
 
     let mut plot = sensoreval_utils::Plot::new("/tmp/sensoreval-plot.html").unwrap();
     let x: Vec<f64> = samples.iter().map(|sample| sample.time_seconds()).collect();
@@ -377,7 +446,7 @@ fn plot(plotopt: &PlotOpt) {
             let mut trace = trace.clone();
             let mut n: usize = 0;
             let mut sum = 0.0;
-            let y: Vec<f64> = y_theta0
+            let y: Vec<_> = y_theta0
                 .iter()
                 .enumerate()
                 .map(|(i, theta0)| {
@@ -411,6 +480,24 @@ fn plot(plotopt: &PlotOpt) {
 
     {
         let mut trace = sensoreval_utils::Plot::default_line();
+        trace.x(&x).name("filtered");
+        trace.line().color(sensoreval_utils::COLOR_E);
+
+        trace.y(&y_theta0d);
+        plot.add_trace_to_rowname_ensure(&mut trace, "gyr-e")
+            .unwrap();
+
+        trace.y(&y_accelx);
+        plot.add_trace_to_rowname_ensure(&mut trace, "acc-n")
+            .unwrap();
+
+        trace.y(&y_accely);
+        plot.add_trace_to_rowname_ensure(&mut trace, "acc-u")
+            .unwrap();
+    }
+
+    {
+        let mut trace = sensoreval_utils::Plot::default_line();
         trace.x(&x).name("[actual]");
         trace.line().color(sensoreval_utils::COLOR_A);
 
@@ -431,7 +518,28 @@ fn plot(plotopt: &PlotOpt) {
             .unwrap();
     }
 
-    // actual: accel data, if the booster is not moving
+    {
+        let mut trace = sensoreval_utils::Plot::default_line();
+        trace.x(&x).name("estimation");
+        trace.line().color(sensoreval_utils::COLOR_E);
+
+        let y: Vec<_> = y_theta0l
+            .iter()
+            .enumerate()
+            .map(|(i, theta0)| theta0 - y_thetab[i])
+            .collect();
+        trace.y(&y);
+        plot.add_trace_to_rowname_ensure(&mut trace, "bo").unwrap();
+
+        let y: Vec<_> = y_theta0d
+            .iter()
+            .map(|theta0d| theta0d - opt.thetabd)
+            .collect();
+        trace.y(&y);
+        plot.add_trace_to_rowname_ensure(&mut trace, "bod").unwrap();
+    }
+
+    // the booster is not moving
     if !opt.thetabd.is_normal() {
         let avg_theta0x = samples
             .iter()
@@ -449,6 +557,7 @@ fn plot(plotopt: &PlotOpt) {
             avg_theta0x, avg_theta0y, avg_theta0
         );
 
+        // actual: accel data
         let mut trace = sensoreval_utils::Plot::default_line();
         trace.x(&x).name("[actual]");
         trace.line().color(sensoreval_utils::COLOR_A);
@@ -462,6 +571,143 @@ fn plot(plotopt: &PlotOpt) {
 
         let y: Vec<f64> = (0..samples.len())
             .map(|_| math::GRAVITY * avg_theta0.cos())
+            .collect();
+        trace.y(&y);
+        plot.add_trace_to_rowname_ensure(&mut trace, "acc-u")
+            .unwrap();
+    }
+
+    // simulator
+    if opt.rc.is_some() && opt.thetac.is_some() {
+        let mut model = sensoreval_psim::models::Booster::new(
+            sensoreval_psim::models::BoosterParams {
+                objects: vec![sensoreval_psim::models::booster::Object {
+                    r: opt.rc.unwrap(),
+                    t: opt.thetac.unwrap(),
+                    m: 1.0,
+                }],
+                rb: opt.rb,
+                thetas: opt.thetas,
+                rs: opt.rs,
+                friction: opt.friction,
+            },
+            0.001,
+        );
+        let mut timedarray = sensoreval_utils::TimedArray::new(&opt.stateupdates);
+
+        // calculate all states
+        let mut states = Vec::with_capacity(samples.len());
+        let mut state = ndarray::array![y_thetab[0], opt.thetabd, y_theta0l[0], y_theta0d[0]];
+        for (i, s) in samples.iter().enumerate() {
+            if i != 0 {
+                model.set_dt(s.time_seconds() - samples[i - 1].time_seconds());
+                model.step(&mut state);
+            }
+
+            states.push(state.clone());
+
+            if let Some(newstate) = timedarray.next(s.time_seconds()) {
+                println!("update @ {} with {:?}", s.time_seconds(), newstate);
+                for (i, val) in newstate.iter().enumerate() {
+                    if !val.is_nan() {
+                        state[i] = *val;
+                    }
+                }
+            }
+        }
+
+        let y_theta0dd_sim: Vec<_> = samples
+            .iter()
+            .enumerate()
+            .map(|(i, _)| {
+                state[State::ThetaB] = y_thetab[i];
+                state[State::ThetaBD] = opt.thetabd;
+                state[State::Theta0] = y_theta0l[i];
+                state[State::Theta0D] = y_theta0d[i];
+
+                if i == 0 {
+                    0.0
+                } else {
+                    model.params().theta0dd(&state)
+                }
+            })
+            .collect();
+        let y_diff: Vec<_> = y_theta0dd_sim
+            .iter()
+            .enumerate()
+            .scan(
+                median::Filter::new(opt.diff_winsz),
+                |filter, (i, theta0dd_sim)| Some(filter.consume_ex(theta0dd_sim - y_theta0dd[i])),
+            )
+            .skip(opt.diff_winsz / 2)
+            .collect::<Vec<_>>()
+            .resize_inplace(samples.len(), 0.0);
+        let y_r: Vec<_> = y_theta0dd_sim
+            .iter()
+            .enumerate()
+            .map(|(i, theta0dd_sim)| theta0dd_sim - y_theta0dd[i])
+            .collect();
+        let y_friction: Vec<_> = samples
+            .iter()
+            .enumerate()
+            .map(|(i, _)| y_diff[i] / (y_theta0d[i] - opt.thetabd))
+            .collect();
+
+        let mut trace = sensoreval_utils::Plot::default_line();
+        trace.x(&x).name("[actual]");
+        trace.line().color(sensoreval_utils::COLOR_A);
+
+        trace.y(&y_theta0dd_sim);
+        plot.add_trace_to_rowname_ensure(&mut trace, &name_theta0dd)
+            .unwrap();
+
+        trace.y(&y_diff);
+        plot.add_trace_to_rowname_ensure(&mut trace, "o-diff")
+            .unwrap();
+
+        trace.y(&y_r);
+        plot.add_trace_to_rowname_ensure(&mut trace, "o-r").unwrap();
+
+        trace.y(&y_friction);
+        plot.add_trace_to_rowname_ensure(&mut trace, "o-fv")
+            .unwrap();
+
+        let mut trace = sensoreval_utils::Plot::default_line();
+        trace.x(&x).name("[sim]");
+        trace.line().color(sensoreval_utils::COLOR_A);
+
+        let y: Vec<f64> = states
+            .iter()
+            .map(|state| math::normalize_angle(state[State::Theta0]))
+            .collect();
+        trace.y(&y);
+        plot.add_trace_to_rowname_ensure(&mut trace, &name_theta0)
+            .unwrap();
+
+        let y: Vec<f64> = states.iter().map(|state| state[State::Theta0D]).collect();
+        trace.y(&y);
+        plot.add_trace_to_rowname_ensure(&mut trace, &name_theta0d)
+            .unwrap();
+
+        let mut accel = ndarray::Array1::zeros(3);
+
+        let y: Vec<f64> = states
+            .iter()
+            .map(|state| {
+                model.to_accel(state, &mut accel);
+                accel[1]
+            })
+            .collect();
+        trace.y(&y);
+        plot.add_trace_to_rowname_ensure(&mut trace, "acc-n")
+            .unwrap();
+
+        let y: Vec<f64> = states
+            .iter()
+            .map(|state| {
+                model.to_accel(state, &mut accel);
+                accel[2]
+            })
             .collect();
         trace.y(&y);
         plot.add_trace_to_rowname_ensure(&mut trace, "acc-u")
