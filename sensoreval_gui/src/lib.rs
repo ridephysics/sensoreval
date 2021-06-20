c730f46a8b589792e983734df660a648aaa9b831
Michael Zimmermann <sigmaepsilon92@gmail.com>
2021-06-20 11:27:11 +0200

    ignore deref_nullptr for bindings
    this is a false-positive because the null pointer is only used to assert the
    field offsets.

--- sensoreval_gui/src/lib.rs
+++ sensoreval_gui/src/lib.rs
@@ -3,6 +3,7 @@ mod native {
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
     #![allow(dead_code)]
+    #![allow(deref_nullptr)]
     #![allow(clippy::redundant_static_lifetimes)]
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
